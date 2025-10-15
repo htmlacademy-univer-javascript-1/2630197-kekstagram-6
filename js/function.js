5541cc676ad453c039d74bbe6ba4dd211948fe9c
Арина Махмадова <arinamahmadova0408@gmail.com>
2025-10-15 16:51:46 +0500

    Функции возвращаются

--- js/function.js
+++ js/function.js
@@ -1,16 +1,11 @@
-function checkStringLength(string, maxLength) {
-  return string.length <= maxLength;
-}
+const meetingWillBe = (startDay, endDay, startMeeting, timeMeeting) => {
+  const separatorStartDay = startDay.indexOf(':');
+  const startDayMinutes = parseInt(startDay.slice(0, separatorStartDay), 10) * 60 + parseInt(startDay.slice(separatorStartDay + 1), 10);
+  const separatorEndDay = endDay.indexOf(':');
+  const endDayMinutes = parseInt(endDay.slice(0, separatorEndDay), 10) * 60 + parseInt(endDay.slice(separatorEndDay + 1), 10);
+  const separatorStartMeeting = startMeeting.indexOf(':');
+  const startMeetingMinutes = parseInt(startMeeting.slice(0, separatorStartMeeting), 10) * 60 + parseInt(startMeeting.slice(separatorStartMeeting + 1), 10);
+  return startMeetingMinutes >= startDayMinutes && startMeetingMinutes <= endDayMinutes && startMeetingMinutes + timeMeeting <= endDayMinutes;
+};
 
-checkStringLength('Строка для проверки', 20);
-
-function isPalindrome(str) {
-  const normalStr = str.replaceAll(' ', '').toLowerCase();
-  let reverStr = '';
-  for (let i = normalStr.length - 1; i >= 0; i--) {
-    reverStr += normalStr[i];
-  }
-  return normalStr === reverStr;
-}
-
-isPalindrome('довод');
+meetingWillBe('08:00', '17:30', '14:00', 90);
