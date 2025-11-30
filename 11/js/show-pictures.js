3648ddcc55864fb5d56e214f4c8f99abce1a706d
Keks <keks@htmlacademy.ru>
2025-11-30 15:40:28 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/show-pictures.js
@@ -0,0 +1,27 @@
+
+const showPicturesTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const container = document.querySelector('.pictures');
+
+const createPictures = ({url, description, likes, comments, id}) => {
+  const showPictures = showPicturesTemplate.cloneNode(true);
+  showPictures.querySelector('.picture__img').src = url;
+  showPictures.querySelector('.picture__img').alt = description;
+  showPictures.querySelector('.picture__likes').textContent = likes;
+  showPictures.querySelector('.picture__comments').textContent = comments.length;
+  showPictures.dataset.pictureId = id;
+  return showPictures;
+};
+
+const renderMiniPictuers = (pictures) => {
+  const similarListFragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    similarListFragment.append(createPictures(picture));
+  });
+
+  container.append(similarListFragment);
+};
+
+export { renderMiniPictuers };
