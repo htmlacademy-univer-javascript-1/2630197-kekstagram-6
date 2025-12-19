42582ddeb6a6abf767bd76a4f197075024e4f6a3
Keks <keks@htmlacademy.ru>
2025-12-19 15:16:35 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/gallery.js
@@ -0,0 +1,25 @@
+import {renderMiniPictuers} from './show-pictures.js';
+import {showBigPicture} from './big-pictures.js';
+
+const container = document.querySelector('.pictures');
+
+let pictures = [];
+
+const onContainerClick = (evt) => {
+  const miniPicture = evt.target.closest('[data-picture-id]');
+  if (!miniPicture) {
+    return;
+  }
+
+  evt.preventDefault();
+  const picture = pictures.find((item) => item.id === +miniPicture.dataset.pictureId);
+  showBigPicture(picture);
+};
+
+const renderGallery = (currentPictures) => {
+  pictures = currentPictures;
+  renderMiniPictuers(pictures, container);
+  container.addEventListener('click', onContainerClick);
+};
+
+export {renderGallery};
---
+++ 14/js/main.js
@@ -0,0 +1,26 @@
+
+import { renderGallery } from './gallery.js';
+import { showSuccessMessage, showErrorMessage } from './message.js';
+import { hideModal, setOnFormSubmit } from './user-form.js';
+import { getData, sendData } from './fetch.js';
+import { showAlert, debounce } from './util.js';
+import { init, getFilterPictures } from './filter.js';
+
+(async () => {
+  try {
+    init(await getData(), debounce(renderGallery));
+    renderGallery(getFilterPictures());
+  } catch (err) {
+    showAlert(err.message);
+  }
+})();
+
+setOnFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    hideModal();
+    showSuccessMessage();
+  } catch (err) {
+    showErrorMessage();
+  }
+});
